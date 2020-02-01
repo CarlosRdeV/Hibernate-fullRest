e85e79c6698db3361372ee23489d7139f1a2059e
Carlos Rivera <carlosrdev@gmail.com>
2020-02-01 14:51:09 -0600

    Update GameRestController.java
    Refactor, name change

--- videogames/src/main/java/com/luv2code/testproject/videogames/rest/GameRestController.java
+++ videogames/src/main/java/com/luv2code/testproject/videogames/rest/GameRestController.java
@@ -1,4 +1,4 @@
-package com.luv2code.testproject.videogames.controller;
+package com.luv2code.testproject.videogames.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
